88953d22ec5253acdebd35e12159743b1fea0c7d
ganeshborole <ganeshborole73@gmail.com>
2026-02-04 15:26:44 +0530

    authtoken issue solve

--- backend/jwt/AuthToken.js
+++ backend/jwt/AuthToken.js
@@ -5,12 +5,14 @@ const createTokenAndSaveCookies = async (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
     expiresIn: "30d",
   });
+
   res.cookie("jwt", token, {
-    httpOnly: true, // Temporarily set to false for testing
-    secure: false,
-    sameSite: "lax",
-    path: "/", // Ensure the cookie is available throughout the site
+    httpOnly: true, // VERY important for security
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    path: "/",
   });
+
   await User.findByIdAndUpdate(userId, { token });
   return token;
 };
