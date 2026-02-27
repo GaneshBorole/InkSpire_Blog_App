4615b90f01638d90f8015bad83bf5a721a1f9943
ganeshborole <ganeshborole73@gmail.com>
2026-02-27 09:34:30 +0530

    project done with deployment

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/logout", isAuthenticated, logout);
+router.get("/logout", logout);
 router.get("/my-profile", isAuthenticated, getMyProfile);
 router.get("/admins", getAdmins);
 
